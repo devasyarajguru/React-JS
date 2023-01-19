9201db98c1f694d14083bbb1384513be90cfe690
Devasya <devasyarajguru@gmail.com>
2023-01-19 16:36:51 +0530

    Arithmetic.js file added

---
+++ Demo/mydemoapp2/src/Arithmetic.js
@@ -0,0 +1,15 @@
+function Arithmetic()
+{
+    var a = 30;
+    var b = 60;
+    return(
+        <div>
+        <h1>{10 + 10}</h1>
+        <h1>A value is {a}</h1>
+        <h1>B value is {b}</h1>
+        <h1>The sum of a and b is {a+b}</h1>
+        </div>
+    );
+}
+
+export default Arithmetic;
