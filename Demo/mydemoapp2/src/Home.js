c4fb093a4dcc3b0d1da32f794dabb23046f6842a
Devasya <devasyarajguru@gmail.com>
2023-01-19 16:39:15 +0530

    Home.js file modified

--- Demo/mydemoapp2/src/Home.js
+++ Demo/mydemoapp2/src/Home.js
@@ -2,7 +2,7 @@ function Home()
 {
     return(
         <div>
-            <h2>
+            <h2 style={{backgroundColor:"orange" , color:"white"}}>
                 Home 
             </h2>
         </div>
