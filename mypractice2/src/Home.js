c806257c167b1d6222bc071f1f298ac0ba0389d3
Devasya <devasyarajguru@gmail.com>
2023-01-23 18:47:36 +0530

    Home.js file added with internal css

---
+++ mypractice2/src/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Home extends React.Component
+{
+    render()
+    {
+        return(
+            <>
+            <h1 style={{backgroundColor:"blue", color:'black'}}>This is Home Page</h1>
+            </>
+        );
+    }
+}
+
+export default Home;
