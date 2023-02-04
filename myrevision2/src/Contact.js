1f310923bd87883e2018c561c95532c4c3bc59ab
Devasya <devasyarajguru@gmail.com>
2023-02-04 15:48:21 +0530

    Contact.js file updated

--- myrevision2/src/Contact.js
+++ myrevision2/src/Contact.js
@@ -1,12 +1,25 @@
+import './style.css';
+import {useNavigate} from 'react-router-dom';
+
 function Contact()
 {
     var mycolor = {
         backgroundColor:'orange',
         color:'black'
     };
+
+    let navigate = useNavigate();
+
+    // function demo1()
+    // {
+    //     navigate('/Home');
+    // }
+    
     return(
         <>
             <h1 style={mycolor}>This is Contact Page</h1>
+            <button onClick={()=>navigate(-1)} className='homebtn'>Backward</button>&nbsp;&nbsp;
+            <button onClick={()=>navigate(1)} className='homebtn'>Forward</button>
         </>
     );
 }
