edd375c65527db2043a1bffb8664ec46948985f5
Devasya <devasyarajguru@gmail.com>
2023-02-04 15:47:52 +0530

    MyHome.js file updated

--- myrevision2/src/MyHome.js
+++ myrevision2/src/MyHome.js
@@ -1,8 +1,19 @@
+import './style.css';
+import {useNavigate} from 'react-router-dom';
+
 function MyHome()
 {
+        let navigate = useNavigate();
+
+        // function demo()
+        // {
+        //     navigate('/Contact');
+        // }
     return(
         <>
             <h1 style={{backgroundColor:'red',color:'white'}}>This is MyHome Page</h1>
+            <button onClick={()=>navigate(-1)} className='homebtn'>Backward</button>&nbsp;&nbsp;
+            <button onClick={()=>navigate(1)} className='homebtn'>Forward</button>
         </>
     );
 }
