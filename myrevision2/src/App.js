5578cf61ce5450df3bb5f3f550c639a0ad5b02b4
Devasya <devasyarajguru@gmail.com>
2023-02-04 15:50:37 +0530

    App.js file updated

--- myrevision2/src/App.js
+++ myrevision2/src/App.js
@@ -1,5 +1,6 @@
 import MyHome from './MyHome';
 import Contact from './Contact';
+import Login from './Login';
 import './style.css';
 import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom';
 
@@ -10,11 +11,14 @@ function App()
       <Router>
         <div className='mycss'>
         <Link to='/MyHome'><button id='btn'>Home</button></Link>&nbsp;&nbsp;&nbsp;
-        <Link to='/Contact'><button id='btn'>Contact</button></Link>
+        <Link to='/Contact'><button id='btn'>Contact</button></Link>&nbsp;&nbsp;&nbsp;
+        <Link to='/Login'><button id='btn'>Login</button></Link>
         </div>
         <Routes>
           <Route path='/MyHome' element={<MyHome/>}></Route>
           <Route path='/Contact' element={<Contact/>}></Route>
+          <Route path='/Login' element={<Login/>}></Route>
+          <Route path='*' element={<MyHome></MyHome>}/>
         </Routes>
       </Router>
 
