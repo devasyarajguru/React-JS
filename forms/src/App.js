1020d86b81197e77156ea9daacdeb368ff9b6926
Devasya <devasyarajguru@gmail.com>
2023-02-07 00:00:13 +0530

    Forms in Reactjs

---
+++ forms/src/App.js
@@ -0,0 +1,13 @@
+// import Input from "./Input";
+// import Name from "./Name";
+
+function App() {
+  return (
+    <>
+      {/* <Input/> */}
+      {/* <Name/> */}
+    </>
+  );
+}
+
+export default App;
