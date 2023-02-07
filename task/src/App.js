d8a4853f949ed9abf5fab2387aa2a41714047c82
Devasya <devasyarajguru@gmail.com>
2023-02-07 18:57:17 +0530

    App.js file added

---
+++ task/src/App.js
@@ -0,0 +1,14 @@
+// import Number from "./Number";
+// import Year from "./Year";
+import Marksheet from "./Marksheet";
+function App() {
+  return (
+      <>
+        {/* <Number/> */}
+        {/* <Year/> */}
+        <Marksheet/>
+      </>    
+  );
+}
+
+export default App;
