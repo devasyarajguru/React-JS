729196e5e239204f11737debd795728bef38c530
Devasya <devasyarajguru@gmail.com>
2023-02-08 17:55:21 +0530

    Small task in React

--- task/src/App.js
+++ task/src/App.js
@@ -1,12 +1,29 @@
-// import Number from "./Number";
-// import Year from "./Year";
-import Marksheet from "./Marksheet";
+//  import Number from "./Number";
+ import Year from "./Year";
+ import Calculator from "./Calculator";
+ import Marksheet from "./Marksheet";
+ import Home from './Home';
+ import { BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
+ import './main.css';
+
 function App() {
   return (
       <>
-        {/* <Number/> */}
-        {/* <Year/> */}
-        <Marksheet/>
+      {/* <Year/> */}
+        <Router>
+          <ul>
+          <li><Link to='/Home'>Home</Link>&nbsp;&nbsp;&nbsp;</li>
+          <li><Link to='/Year'>Year</Link>&nbsp;&nbsp;&nbsp;</li>
+          <li><Link to='/Marksheet'>Marksheet</Link>&nbsp;&nbsp;&nbsp;</li>
+          <li><Link to='/Calculator'>Calculator</Link></li>
+          </ul>
+          <Routes>
+            <Route path="/Home" element={<Home></Home>}></Route>
+            <Route path="/Year" element={<Year/>}></Route>
+            <Route path="/Calculator" element={<Calculator/>}></Route>
+            <Route path="/Marksheet" element={<Marksheet/>}></Route>
+          </Routes>
+        </Router>
       </>    
   );
 }
