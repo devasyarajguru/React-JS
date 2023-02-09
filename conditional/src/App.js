839abb3f4a777ba37c76db686ca600cfc224c263
Devasya <devasyarajguru@gmail.com>
2023-02-09 16:09:29 +0530

    App.js file added

---
+++ conditional/src/App.js
@@ -0,0 +1,18 @@
+// import Condition from "./Condition";
+// import Condition3 from "./Condition3";
+// import Condition4 from "./Condition4";
+
+import Condition5 from "./Condition5";
+
+function App() {
+  return (
+    <>
+      {/* <Condition/> */}
+      {/* <Condition3/> */}
+      {/* <Condition4/> */}
+      <Condition5/>
+    </>
+  );
+}
+
+export default App;
