74373875d77770cc3263891e46c088634c6e0ffd
Devasya <devasyarajguru@gmail.com>
2023-02-18 14:54:10 +0530

    Hooks in React

---
+++ hooks/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import Hook1 from './Hook1';
+// import Hook2 from './Hook2';
+// import Hook3 from './Hook3';
+// import Counter from './Counter';
+// import Hook4 from './Hook4';
+import Sum from './Sum';
+
+function App(props) {
+  return (
+    <div> 
+        {/* <Hook1></Hook1> */}
+        {/* <Hook2/> */}
+        {/* <Hook3/> */}
+        {/* <Counter/> */}
+        {/* <Hook4/> */}
+        <Sum/>
+    </div>
+  );
+}
+
+export default App;
