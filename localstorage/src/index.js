18635bcfe181a77c3b7420956d81204db8b37cf3
Devasya <devasyarajguru@gmail.com>
2023-02-21 19:06:27 +0530

    LocalStorage in React

---
+++ localstorage/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App3 from './App3';
+// import App4 from './App4';
+import App5 from './App5';
+import './index.css';
+// import App from './App';
+// import App2 from './App2';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <App2/> */}
+    {/* <App3></App3> */}
+    {/* <App4/> */}
+    <App5/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
