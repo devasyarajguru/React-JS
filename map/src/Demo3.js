217d07296dfd9b60180656f8112b63190d068015
Devasya <devasyarajguru@gmail.com>
2023-02-28 15:55:07 +0530

    Map in React

---
+++ map/src/Demo3.js
@@ -0,0 +1,14 @@
+let arr = [1,2,3,4,5];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+
+
+
+for(var i=0; i<arr.length; i++)
+{
+    console.log(arr[i]);
+}
