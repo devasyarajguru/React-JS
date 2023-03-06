8ac013d0978ba6b44a0b94a7fe4cf77d788096eb
Devasya <devasyarajguru@gmail.com>
2023-03-06 16:41:46 +0530

    API calling in React JS

---
+++ api/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App5 from './App5';
+import App6 from './App6';
+// import App4 from './App4';
+// import App3 from './App3';
+import './index.css';
+// import App from './App';
+// import App2 from './App2';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+    // <App />
+    // <App2/>
+    // <App3/>
+    // <App4/>
+    // <App5/>
+    <App6/>
+  
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
