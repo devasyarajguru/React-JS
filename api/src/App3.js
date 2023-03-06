63db29f12fa9783e405e6247effbe423ac2a9d0b
Devasya <devasyarajguru@gmail.com>
2023-03-06 16:45:34 +0530

    API calling in React JS

---
+++ api/src/App3.js
@@ -0,0 +1,50 @@
+import React from 'react';
+class App3 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            myarr:[]
+         };
+    }
+
+    componentDidMount() {
+        fetch('https://fakestoreapi.com/products')
+        .then(dev => dev.json())
+        .then(dev => 
+            {
+                // console.log(dev);
+                this.setState({myarr:dev});
+            }
+            
+        )
+    }
+
+    render() {
+        return (
+            <>
+         
+            {this.state.myarr.map(value =>
+            (
+                <table border='1'>
+
+                        <tr>
+                            <td>{value.id}</td>
+                            <td>{value.title}</td>
+                            <td>{value.category}</td>
+                            <td>{value.rating.rate}</td>
+                            <td>{value.rating.count}</td>
+                            <img src='{value.image}'></img>
+                        </tr>
+                    </table>
+             
+             
+                  
+            )
+                
+            )}
+            </>
+        );
+    }
+}
+
+export default App3;
