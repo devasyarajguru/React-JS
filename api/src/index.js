3d8242ea3a7bd2f25a747da8293e535ca2a3793b
Devasya <devasyarajguru@gmail.com>
2023-03-07 14:30:30 +0530

    API calling in React JS

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App5 from './App5';
-import App6 from './App6';
+// import App7 from './App7';
+// import App5 from './App5';
+// import App6 from './App6';
 // import App4 from './App4';
-// import App3 from './App3';
+// import App8 from './App8';
+import App3 from './App3';
 import './index.css';
 // import App from './App';
 // import App2 from './App2';
@@ -14,10 +16,12 @@ root.render(
   
     // <App />
     // <App2/>
-    // <App3/>
+    <App3/>
     // <App4/>
     // <App5/>
-    <App6/>
+    // <App6/>
+    // <App7/>
+    // <App8/>
   
 );
 
